e7288aa807ac5de51f3cf22db9b29f80fb45799f
Isashi <isashimitsui@gmail.com>
2017-09-01 16:27:11 +0200

    almost finish css

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :posts, only: [:new, :create, :index]
 
-  root   'posts#index'
+  root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
